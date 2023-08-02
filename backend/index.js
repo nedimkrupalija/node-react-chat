dfa83eb21915a1db942616818ed7af32f6a0adc0
nedimkrupalija <nkrupalija1@etf.unsa.ba>
2023-08-02 19:08:01 +0200

    finished node backend

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const {default: axios} = require("axios");
 
 const app = express();
 app.use(express.json());
@@ -9,15 +10,17 @@ app.use(cors({ origin: true }));
 
 app.post("/authenticate", async (req, res) => {
   const { username } = req.body;
-    try {
-        const r = await axios.put("https://api.chatengine.io/users/",
-            { username: username, secret: username, first_name: username },
-            { headers: { "private-key": "7fa989fa-5c39-4081-ae2b-7e4dcd48f103" } }
-        );
-        return res.status(r.status).json(r.data);
-    } catch (e) {
-        return res.status(e.response).json(e.response);
-    }
+  try{
+    const r = await axios.put("https://api.chatengine.io/users/",
+    {username: username, secret: username, first_name: username},
+    {headers: {"private-key": "7fa989fa-5c39-4081-ae2b-7e4dcd48f103"}}
+    );
+    return res.status(r.status).json(r.data);
+  } 
+  catch(e){
+    return res.status(e.response.status).json(e.response.data);
+  }
+
 });
 
 app.listen(3001);
